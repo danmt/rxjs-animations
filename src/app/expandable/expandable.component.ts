3c3a91b9c93b5a2bf78f846c5c98d938eb8afacc
Daniel Marin <danielarturomt@gmail.com>
2020-01-07 21:22:20 -0300

    minor adjustments

--- src/app/expandable/expandable.component.ts
+++ src/app/expandable/expandable.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
-import { NgxExpandableComponent } from 'ngx-expandable';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-expandable',
   templateUrl: './expandable.component.html',
   styleUrls: ['./expandable.component.scss']
 })
-export class ExpandableComponent implements OnInit {
-  @ViewChild(NgxExpandableComponent, { static: true }) expandable: any;
-  expanded$: Observable<boolean>;
-  collapsed$: Observable<boolean>;
-  inProgress$: Observable<boolean>;
-  expanding$: Observable<boolean>;
-  collapsing$: Observable<boolean>;
+export class ExpandableComponent {
   collapsedHeight = 40;
   expandedHeight = 400;
-
-  constructor() {}
-
-  ngOnInit() {
-    this.expanded$ = this.expandable.expanded$;
-    this.collapsed$ = this.expandable.collapsed$;
-    this.inProgress$ = this.expandable.inProgress$;
-    this.expanding$ = this.expandable.expanding$;
-    this.collapsing$ = this.expandable.collapsing$;
-  }
 }
